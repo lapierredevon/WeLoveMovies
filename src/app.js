b5503738952f910dde4253966dcda5ba014304ad
Devon Lapierre <lapierredevon@gmail.com>
2022-12-16 12:00:44 -0500

    enable-cors

--- src/app.js
+++ src/app.js
@@ -1,14 +1,15 @@
 if (process.env.USER) require("dotenv").config();
 
 const express = require("express");
+const app = express();
 const movieListRouter = require("../src/movies_list/movies_list.routes");
 const reviewRouter = require("../src/reviews/reviews.routes");
 const theatersRouter = require("../src/theaters/theater.routes");
+const cors = require("cors");
 
-const app = express();
-
+// app.use cors enables cross origin resource sharing
+app.use(cors());
 app.use(express.json());
-
 app.use("/movies", movieListRouter);
 app.use("/reviews", reviewRouter);
 app.use("/theaters", theatersRouter);
